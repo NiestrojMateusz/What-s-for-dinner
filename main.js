aac00b9205fdcd4b9aaabba2697d72ead9d26bcf
Julka <jwylezo@gmail.com>
2020-08-18 10:47:19 +0200

    add connecting with API

--- main.js
+++ main.js
@@ -1,35 +1,44 @@
-// const auth = "194c879ee3354f51bfca24dfb859cd08";
-
-// async function apiFunc() {
-//   const dataFetch = await fetch("https://api.spoonacular.com/recipes/complexSearch?query=pasta&maxFat=25&number=2&apiKey=194c879ee3354f51bfca24dfb859cd08",
-// {
-//   method: "GET",
-//   headers: {
-//     Accept: "application/json",
-//     // Authorization: auth
-//   }
-// }
-//   );  
-//   const data = await dataFetch.json();
-//   console.log(data.results);
-// }
-
-// apiFunc()
-
-
-
 const title = document.querySelector('.title');
-const input = document.querySelector('.title-show');
-document.querySelector('.search').addEventListener('click', animateSearch);
+const searchInput = document.querySelector('.title-show');
+const searchIcon = document.querySelector('.fa-search');
+const searchBtn = document.querySelector('.search');
+const revealButton = document.querySelector('.reveal-input');
+
+// Reveal searching input
 
 function animateSearch(e) {
-  input.classList.add('input-active');
+  searchInput.classList.add('input-active');
   document.querySelectorAll('.title-hide').forEach(span => {
     span.style.display = 'none';
   });
+  revealButton.classList.add('search-hide');
 };
 
-input.addEventListener('animationstart', () => {
-  title.querySelector('.svg-inline--fa').classList.add('icon-active');
-  input.focus();
-})
+// API download
+
+const apiKey = "&apiKey=194c879ee3354f51bfca24dfb859cd08";
+
+async function apiFunc() {
+  const searchValue = searchInput.value;
+  const dataFetch = await fetch(`https://api.spoonacular.com/recipes/complexSearch?query=${searchValue}${apiKey}`,
+{
+  method: "GET",
+  headers: {
+    Accept: "application/json",
+  }
+}
+  );  
+  const data = await dataFetch.json();
+  console.log(data.results);
+};
+
+
+
+// Event listeners and invokes
+
+revealButton.addEventListener('click', animateSearch);
+searchInput.addEventListener('animationstart', () => {
+ searchBtn.classList.add('icon-active');
+  searchInput.focus();
+});
+searchBtn.addEventListener('click', apiFunc);
