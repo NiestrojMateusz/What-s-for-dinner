c42627ea2bd65d3246bfefca5f40c98ef6954e4e
Julka <jwylezo@gmail.com>
2020-08-25 10:38:17 +0200

    download info about recipes from api

--- main.js
+++ main.js
@@ -1,8 +1,11 @@
+import { data } from './data.js';
 const title = document.querySelector('.title');
 const searchInput = document.querySelector('.title-show');
 const searchIcon = document.querySelector('.fa-search');
 const searchBtn = document.querySelector('.search');
 const revealButton = document.querySelector('.reveal-input');
+
+const test = {}
 
 // Reveal searching input
 
@@ -18,6 +21,7 @@ function animateSearch(e) {
 // API download
 
 const apiKey = "&apiKey=194c879ee3354f51bfca24dfb859cd08";
+let recipesId= [];
 
 async function apiFunc() {
   const searchValue = searchInput.value;
@@ -34,45 +38,157 @@ async function apiFunc() {
 
   // Get recipe ID
 
-  let recipesId = [];
+ 
   recipes.forEach(recipe => {
     let id = recipe.id;
     recipesId.push(id);
   });
 
-  getRecipesInfo(recipesId);
+  // getRecipesInfo(recipesId);
 };
 
 // Get recipe info
 
-// async function getRecipesInfo(arrayId) {
-//   const resipesInfo = [];
-//   arrayId.forEach(id => {
-//     const dataFetch = await fetch(`https://api.spoonacular.com/recipes/${id}?${apiKey}`,
-//     {
-//       method: "GET",
-//       headers: {
-//         Accept: "application/json",
-//       }
-//     }
-//       );  
-//       const data = await dataFetch.json();
-//       resipesInfo.push(data);
-//   })
-// }
+const getRecipesInfo = async arrayId => {
+  let recipesInfo = [];
 
-// Changing vue to searched recipes
+  for(let i = 0; i < arrayId.length; i++) {
+    const id = arrayId[i];
+    const infoFetch = await fetch(`https://api.spoonacular.com/recipes/${id}/information?${apiKey}`,
+    {
+      method: "GET",
+      headers: {
+        Accept: "application/json",
+      }
+    }
+      );  
+      const data = await infoFetch.json();
+      recipesInfo.push(data);
+  }
+  recipeView(recipesInfo);
+  // console.log(recipesInfo[0]);
+  }
+  
+// Changing view to searched recipes
 
 function revealRecpies() {
   // apiFunc();
-  recipeVue();
-}
-
-function recipeVue() {
+  // recipeView();
   searchInput.value = '';
 }
 
+function recipeView(infoArray) {
+  const recipesContainer = document.querySelector('.recipes-container');
+  infoArray.forEach(info => {
+    const recipeSection = document.createElement('section');
+    recipeSection.classList.add('recipe');
+    recipesContainer.appendChild(recipeSection);
 
+    const recipeImg = document.createElement('div');
+    recipeImg.classList.add('recipe-img');
+    let url = info.image;
+    recipeImg.style.backgroundImage = `url("${url}")`;
+    recipeSection.appendChild(recipeImg);
+
+    const recipeInfo = document.createElement('div');
+    recipeInfo.classList.add('recipe-info');
+    recipeSection.appendChild(recipeInfo);
+
+    const recipeTitle = document.createElement('h3');
+    recipeTitle.classList.add('recipe-title');
+    let title = info.title;
+    recipeTitle.innerText = title;
+    recipeInfo.appendChild(recipeTitle);
+
+    const recipeTime = document.createElement('div');
+    recipeTime.classList.add('recipe-time');
+    recipeInfo.appendChild(recipeTime);
+
+    const time = document.createElement('h4');
+    time.classList.add('time');
+    let minutes = info.readyInMinutes;
+    time.innerHTML = `<i class='fas fa-clock'></i> ${minutes} min`;
+    recipeTime.appendChild(time);
+
+    const money = document.createElement('span');
+    let price = info.cheap;
+    money.innerHTML = `<i class="fas fa-dollar-sign"></i> ${price? 'Cheap':'Expensive'}`;
+    recipeTime.appendChild(money);
+
+    const recipeLink = document.createElement('div');
+    recipeLink.classList.add('recipe-link');
+    const link = document.createElement('a');
+    recipeLink.appendChild(link);
+    const hrefValue = info.sourceUrl;
+    link.setAttribute('href',hrefValue);
+    link.setAttribute('target', "_blank");
+    link.innerHTML = `<i class="fas fa-link"></i> Go to recipe`
+    recipeInfo.appendChild(recipeLink);
+
+    const recipeNutrition = document.createElement('div');
+    recipeNutrition.classList.add('recipe-nutrition');
+    recipeSection.appendChild(recipeNutrition);
+
+    const nutritionFacts = [info.dairyFree, info.glutenFree, info.vegan, info.veryHealthy, info.healthScore];
+    if(!nutritionFacts[0]) {
+      const fact = document.createElement('span');
+      fact.classList.add('nutrition-fact');
+      fact.innerHTML = `<i class="fas fa-cheese"></i> Dairy Free <span class='false-fact'></span>`;
+      recipeNutrition.appendChild(fact);
+    } else if(nutritionFacts[0]) {
+      const fact = document.createElement('span');
+      fact.classList.add('nutrition-fact');
+      fact.innerHTML = `<i class="fas fa-cheese"></i> Dairy Free`;
+      recipeNutrition.appendChild(fact);
+    };
+
+    if(!nutritionFacts[1]) {
+      const fact = document.createElement('span');
+      fact.classList.add('nutrition-fact');
+      fact.innerHTML = `<i class="fas fa-bread-slice"></i> Gluten Free <span class='false-fact'></span>`;
+      recipeNutrition.appendChild(fact);
+    } else if(nutritionFacts[1]) {
+      const fact = document.createElement('span');
+      fact.classList.add('nutrition-fact');
+      fact.innerHTML = `<i class="fas fa-bread-slice"></i> Gluten Free`;
+      recipeNutrition.appendChild(fact);
+    };
+
+    if(!nutritionFacts[2]) {
+      const fact = document.createElement('span');
+      fact.classList.add('nutrition-fact');
+      fact.innerHTML = `<i class="fas fa-seedling"></i> Vegan <span class='false-fact'></span>`;
+      recipeNutrition.appendChild(fact);
+    } else if(nutritionFacts[2]) {
+      const fact = document.createElement('span');
+      fact.classList.add('nutrition-fact');
+      fact.innerHTML = `<i class="fas fa-seedling"></i> Vegan`;
+      recipeNutrition.appendChild(fact);
+    };
+
+    if(!nutritionFacts[3]) {
+      const fact = document.createElement('span');
+      fact.classList.add('nutrition-fact');
+      fact.innerHTML = `<i class="fas fa-heartbeat"></i>  <span class='false-fact'></span>`;
+      recipeNutrition.appendChild(fact);
+    } else if(nutritionFacts[3]) {
+      const fact = document.createElement('span');
+      fact.classList.add('nutrition-fact');
+      fact.innerHTML = `<i class="fas fa-heartbeat"></i> Healthy`;
+      recipeNutrition.appendChild(fact);
+    };
+
+    if(nutritionFacts[4] >= 0) {
+      const fact = document.createElement('span');
+      fact.classList.add('nutrition-fact');
+      fact.innerHTML = `<span class="score">${nutritionFacts[4]}</span> Health Score `;
+      recipeNutrition.appendChild(fact);
+    };
+
+  })
+}
+
+recipeView([data]);
 
 // Event listeners and invokes
 
@@ -82,3 +198,5 @@ searchInput.addEventListener('animationstart', () => {
   searchInput.focus();
 });
 searchBtn.addEventListener('click', revealRecpies);
+
+export default test;
